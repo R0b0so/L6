b715c6ce96132498e6eceedfc635a1147b9785d5
R0b0so <ethansagar222@gmail.com>
2024-10-09 19:57:29 -0700

    hello

---
+++ NOT.py
@@ -0,0 +1,11 @@
+A = 8
+B = 9
+C = 8
+if A != B:
+   print("a is not equal to b")
+if A != C:
+   print("a is not equal to c")
+else:
+   print("a is equal to c")
+if (B == 8) != (A == 8):
+ print("b is not equal to 8")
