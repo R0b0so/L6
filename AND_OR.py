5ca2ef0052b754faa2ab872f0660d10828683fe0
R0b0so <ethansagar222@gmail.com>
2024-10-09 19:50:24 -0700

    hello

---
+++ AND_OR.py
@@ -0,0 +1,12 @@
+a = 5
+b = 3
+c = 6
+d = 7
+if a and b: 
+  print("Both have some value")
+else:
+  print("Both have 0")
+if c or d:
+  print("Atleast one has value")
+else: 
+  print("Neither have value")
